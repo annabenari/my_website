48d35e070cd3bd0411b7924f755203accb7742d0
annabenari <annabenari@me.com>
2024-11-17 20:39:39 +0000

    got rid of the carasoul

--- script.js
+++ script.js
@@ -47,80 +47,12 @@ const contentData = {
   <h2>Creative Facilitation</h2>
     <h4>Print Making Workshop, Wysing Art Centre - November 2024</h4>
     <p>This session was a hands-on workshop focused on creative printmaking with natural elements. Participants explored basic techniques using found objects from nature, encouraging creativity and experimentation in a supportive environment.</p>
-
-  //
-  <!-- Container for the image gallery -->
-<div class="container">
-
-  <!-- Full-width images with number text -->
-  <div class="mySlides">
-    <div class="numbertext">1 / 6</div>
-      <img src="../art_work/flower.png" class="flower">
-  </div>
-
-  <div class="mySlides">
-    <div class="numbertext">2 / 6</div>
-      <img src="../art_work/flower.png" class="flower">
-  </div>
-
-  <div class="mySlides">
-    <div class="numbertext">3 / 6</div>
-      <img src="../art_work/flower.png" class="flower">
-  </div>
-
-  <div class="mySlides">
-    <div class="numbertext">4 / 6</div>
-      <img src="../art_work/flower.png" class="flower">
-  </div>
-
-  <div class="mySlides">
-    <div class="numbertext">5 / 6</div>
-      <img src="../art_work/flower.png" class="flower">
-  </div>
-
-  <div class="mySlides">
-    <div class="numbertext">6 / 6</div>
-      <img src="../art_work/flower.png" class="flower">
-  </div>
-
-  <!-- Next and previous buttons -->
-  <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-  <a class="next" onclick="plusSlides(1)">&#10095;</a>
-
-  <!-- Image text -->
-  <div class="caption-container">
-    <p id="caption"></p>
-  </div>
-
-  <!-- Thumbnail images -->
-  <div class="row">
-    <div class="column">
-      <img class="demo cursor" src="../art_work/flower.png" style="width:100%" onclick="currentSlide(1)">
-    </div>
-    <div class="column">
-      <img class="demo cursor" src="../art_work/flower.png" style="width:100%" onclick="currentSlide(2)">
-    </div>
-    <div class="column">
-      <img class="demo cursor" src="../art_work/flower.png" style="width:100%" onclick="currentSlide(3)" >
-    </div>
-    <div class="column">
-      <img class="demo cursor" src="../art_work/flower.png" style="width:100%" onclick="currentSlide(4)" >
-    </div>
-    <div class="column">
-      <img class="demo cursor" src="../art_work/flower.png" style="width:100%" onclick="currentSlide(5)" >
-    </div>
-    <div class="column">
-      <img class="demo cursor" src="../art_work/flower.png" style="width:100%" onclick="currentSlide(6)" >
-    </div>
-  </div>
-</div>
-
-//
-
-
-
-
+    <img class="wysing-images" src="../wysing-art-centre-1/1.JPG" alt="">
+    <img class="wysing-images" src="../wysing-art-centre-1/2.JPG" alt="">
+    <img class="wysing-images" src="../wysing-art-centre-1/6.JPG" alt="">
+ 
     <br>
+
     <h4>'Play, Build Play', Fitzwilliam Museum - August 2024</h4>
     <p>
       As facilitator of the Summer Play Pavilion, I engaged with families to foster an inviting and playful environment. I guided participants in exploring the curated collection of loose parts, encouraging creative play, collaboration, and the development of their own sporting and cultural activities.
@@ -166,38 +98,3 @@ document.querySelectorAll("[data-content]").forEach((link) => {
     contentDiv.innerHTML = contentData[contentKey];
   });
 });
-
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("demo");
-  let captionText = document.getElementById("caption");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-  captionText.innerHTML = dots[slideIndex - 1].alt;
-}
