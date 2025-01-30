2f82916dbccb52a73f67048adf58543a6a024adf
annabenari <annabenari@me.com>
2025-01-30 19:40:10 +0000

    updated current

--- current.js
+++ current.js
@@ -1,13 +1,34 @@
+// export const current = {
+//   current: `
+//   <h3>Babylon Arts: Small but Perfect </h3>
+//   <p>On until the 22nd December , all of my peices are for sale for £60</p>
+//   <img class="current" src="../current/1.jpg" alt="">
+//   <img class="current" src="../current/5.jpg" alt="">
+//   <img class="current" src="../current/2.jpg" alt="">
+//   <img class="current" src="../current/6.jpg" alt="">
+//   <img class="current" src="../current/3.jpg" alt="">
+//   <img class="current" src="../current/8.jpg" alt="">
+//   <img class="current" src="../current/7.jpg" alt="">
+//   `,
+// };
+
 export const current = {
   current: `
-  <h3>Babylon Arts: Small but Perfect </h3>
-  <p>On until the 22nd December , all of my peices are for sale for £60</p>
-  <img class="current" src="../current/1.jpg" alt="">
-  <img class="current" src="../current/5.jpg" alt="">
-  <img class="current" src="../current/2.jpg" alt="">
-  <img class="current" src="../current/6.jpg" alt="">
-  <img class="current" src="../current/3.jpg" alt="">
-  <img class="current" src="../current/8.jpg" alt="">
-  <img class="current" src="../current/7.jpg" alt="">
+<h3>TLP work shops</h3>
+<p>Create unique monoprints using leaves, flowers and other natural materials. Anna Benari will introduce the techniques needed then give you the creative freedom to experiment. You'll leave with beautiful prints and a newfound appreciation of nature's textures.</p>
+<p>Location: Soham Library, 7 Clay St, Soham, CB7 5HJ<br>
+When: Saturday 17th May 10.30am<br>
+Running time:  2 hour workshop<br>
+Suitable for: Adults and young people aged 5+ yrs <br>
+Tickets: £6.50 / £3 <br>
+<a href="https://www.library.live/the-library-presents-events/cecilyandsalty-bzc8z-a5kc3-dpayk-8tg2j-tx84f-5sep6" target="_blank" class="link-reference">To book follow this link</a><br><br>
+or <br><br>
+Location: Alconbury Weald Cricket Pavillion, The Boulevard, Alconbury Weald, PE28 4XB<br>
+When: Thursday 10th April 2pm<br>
+Running time:  2 hour workshop<br>
+Suitable for: Adults and young people aged 5+ yrs <br>
+Tickets: £6.50 / £3 <br><br>
+<a href="https://www.library.live/the-library-presents-events/cecilyandsalty-bzc8z-a5kc3-dpayk-8tg2j-tx84f" target="_blank" class="link-reference">To book follow this link</a>
+</p>
   `,
 };
