863c78d24e47b236b3cd15f38479a5b372c65d40
annabenari <annabenari@me.com>
2025-01-30 19:50:43 +0000

    updated current

--- current.js
+++ current.js
@@ -20,7 +20,7 @@ export const current = {
 When: Saturday 17th May 10.30am<br>
 Running time:  2 hour workshop<br>
 Suitable for: Adults and young people aged 5+ yrs <br>
-Tickets: £6.50 / £3 <br>
+Tickets: £6.50 / £3 <br><br>
 <a href="https://www.library.live/the-library-presents-events/cecilyandsalty-bzc8z-a5kc3-dpayk-8tg2j-tx84f-5sep6" target="_blank" class="link-reference">To book follow this link</a><br><br>
 or <br><br>
 Location: Alconbury Weald Cricket Pavillion, The Boulevard, Alconbury Weald, PE28 4XB<br>
@@ -28,7 +28,17 @@ When: Thursday 10th April 2pm<br>
 Running time:  2 hour workshop<br>
 Suitable for: Adults and young people aged 5+ yrs <br>
 Tickets: £6.50 / £3 <br><br>
-<a href="https://www.library.live/the-library-presents-events/cecilyandsalty-bzc8z-a5kc3-dpayk-8tg2j-tx84f" target="_blank" class="link-reference">To book follow this link</a>
+<a href="https://www.library.live/the-library-presents-events/cecilyandsalty-bzc8z-a5kc3-dpayk-8tg2j-tx84f" target="_blank" class="link-reference">To book follow this link</a><br><br><br><br><br><br>
+</p> 
+<h3>Metamorphosis Exhibition</h3>
+<p> Artist statment : I am an artist and curator passionate about human expressions—sometimes distorted—and the beauty of nature. My work explores the connection between women, femininity, and nature, viewing the woman’s form as integral to the natural world. This inspires holistic pieces that weave these themes into a cohesive narrative. Dedicated to learning and creative growth, I seek new ways to expand my artistic repertoire. I strive to connect with audiences through exhibitions reflecting my evolving perspective. My curatorial experience includes Babylon Arts and Norwich & Norfolk Festival, co-curating exhibitions, selecting artists, overseeing installations, and ensuring high standards throughout.</p>
+<p>Date:Tuesday 4th February - Sunday 9th February <br>
+Speical date: Open view: Thursday 6th February, 18:00-20:00<br>
+Location: Anteros Arts Foundation, 7-15 Fye Bridge, Norwich<br>
+<img class="images" src="" alt="">
+<img class="images" src="" alt="">
+<img class="images" src="./art_work/2.jpeg" alt="" This will not be for sale>
+
 </p>
   `,
 };
