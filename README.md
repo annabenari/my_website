7eab15894b78e7f69d5189da4ff9431030e39d91
annabenari <annabenari@me.com>
2025-02-11 17:34:09 +0000

    web dev stuff

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # my_website
+
+To make this websit live locally please click "go live" on Visual Studio Code
--- web.js
+++ web.js
@@ -1,3 +1,88 @@
 export const webContent = {
-  web: ``,
+  web: `
+  <h2>My Coding Journey</h2>
+
+  <section>
+    <article>
+      <h3>I Created This Website</h3>
+      <p>Check out the repository for this project on my GitHub: 
+        <a href="https://github.com/annabenari/my_website" target="_blank" class="link-reference">Click here</a>
+      </p>
+    </article>
+  </section>
+
+  <section>
+    <article>
+      <h3>Udemy Python Course</h3>
+      <p>Check out my repository:
+        <a href="https://github.com/annabenari/100-days-python" target="_blank" class="link-reference">Click here</a>
+      </p>
+      <p>See the course details:
+        <a href="https://www.udemy.com/course/100-days-of-code/" target="_blank" class="link-reference">Click here</a>
+      </p>
+    </article>
+  </section>
+
+  <section>
+    <article>
+      <h3>Udemy JavaScript Course</h3>
+      <p>Check out my repository:
+        <a href="https://github.com/annabenari/complete-javascript-course" target="_blank" class="link-reference">Click here</a>
+      </p>
+      <p>See the course details:
+        <a href="https://www.udemy.com/course/the-complete-javascript-course/" target="_blank" class="link-reference">Click here</a>
+      </p>
+    </article>
+  </section>
+
+  <section>
+    <article>
+      <h3>Client Website</h3>
+      <p>Check out the repository:
+        <a href="https://github.com/annabenari/barbaraWebsite" target="_blank" class="link-reference">Click here</a>
+      </p>
+    </article>
+  </section>
+
+  <section>
+    <article>
+      <h3>BotaniBuddy App (Back-End)</h3>
+      <p>Check out the repository:
+        <a href="https://github.com/annabenari/BotaniBuddy-BE" target="_blank" class="link-reference">Click here</a>
+      </p>
+    </article>
+  </section>
+
+  <section>
+    <article>
+      <h3>BotaniBuddy App (Front-End)</h3>
+      <p>Check out the repository:
+        <a href="https://github.com/annabenari/BotaniBuddy-FE" target="_blank" class="link-reference">Click here</a>
+      </p>
+    </article>
+  </section>
+
+  <section>
+    <article>
+      <h3>Code First Girls Course</h3>
+      <p>I completed a JavaScript & Front-End course. Learn more:
+        <a href="https://codefirstgirls.com/" target="_blank" class="link-reference">Click here</a>
+      </p>
+    </article>
+  </section>
+
+  <section>
+    <article>
+      <h3>HTML & CSS Practice Task (Code First Girls)</h3>
+      <p>Check out my repository:
+        <a href="https://github.com/annabenari/ck24ukh2" target="_blank" class="link-reference">Click here</a>
+      </p>
+    </article>
+  </section>
+
+
+
+
+  
+  `,
 };
